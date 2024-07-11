fa5ebd739241f071c34dfd7564f45a73a79c9acf
dengidengi <dengianassi@gmail.com>
2024-07-11 15:26:26 +0200

    Links added

---
+++ src/pages/map.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Map() {
+    return (
+        <>
+        <div>Map component</div>
+        <div className="flex justify-center items-center">
+
+        </div>
+        </>
+    );
+}
