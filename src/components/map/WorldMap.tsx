d0f3df334b794fa4fc0d4f250a56cf1e9a800014
dengidengi <dengianassi@gmail.com>
2024-07-13 17:49:07 +0200

    Changed number ids to country codes in topoJson file

--- src/components/map/WorldMap.tsx
+++ src/components/map/WorldMap.tsx
@@ -1,24 +1,28 @@
-import React, { useEffect } from "react"
+import React, { useState,useEffect } from "react"
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
-import topoJson from '@/../public/topojson/countries50m.json'
+import topoJson from '@/../public/topojson/new_countries50m.json'
 
 
 export default function WorldMap() {
+    const [countryName,setCountryName] = useState('');
     useEffect(() => {
         console.log(topoJson);
     },[])
     return (
+        <>
+        <div>{countryName}</div>
         <div className="w-screen h-5/6">
         <ComposableMap projection="geoMercator">
             <Geographies geography={topoJson}>
                 { // "Function as children / render prop" pattern
                     ({ geographies }) => 
                         geographies.map((geo) => (
-                            <Geography key={geo.rsmKey} geography={geo} />
+                            <Geography key={geo.rsmKey} geography={geo} onMouseEnter={() => setCountryName(geo.properties.name)}/>
                         ))
                 }
             </Geographies>
         </ComposableMap>
         </div>
+        </>
     );
 }
